7da71b09bdc2ec91fa5fce0b87c4562cd5434fef
PeaceComedy <baoyugang00@gmail.com>
2025-12-02 17:28:00 +0800

    1

--- main/main.gd
+++ main/main.gd
@@ -4,11 +4,15 @@ extends Node
 @export var game_over_menu_scene: PackedScene # 死亡菜单
 @export var default_player: PackedScene # 默认玩家
 @export var first_level: PackedScene # 初始关卡
+@export var hud_scene: PackedScene # 游戏内ui
+
 @onready var world_container: Node2D = $WorldContainer
 @onready var gui: CanvasLayer = $GUI
+@onready var hud: CanvasLayer = $HUD
 
 var current_main_menu_instance: Node = null # 用来存储当前的主菜单实例
 var current_game_over_menu: Node = null # 用来存储当前打开的死亡菜单
+
 
 func _ready():
 	GlobalPlayerManager.player_scene = default_player # 初始化全局玩家配置
@@ -16,46 +20,51 @@ func _ready():
 	GlobalPlayerManager.player_died.connect(_on_player_died) # 监听玩家死亡
 	load_main_menu() # 加载主菜单
 
+
 func load_main_menu(): # 加载主菜单
 	_clear_world() # 清空旧关卡
-	if main_menu_scene:
-		current_main_menu_instance = main_menu_scene.instantiate()
-		gui.add_child(current_main_menu_instance)
-		# 连接信号：当菜单发出"start_game"时，执行_on_game_started
-		if current_main_menu_instance.has_signal("start_game"): 
-			current_main_menu_instance.start_game.connect(_on_game_start)
+	_clear_hud() # 清空旧ui
+	
+	current_main_menu_instance = main_menu_scene.instantiate()
+	gui.add_child(current_main_menu_instance)
+	# 连接信号：当菜单发出"start_game"时，执行_on_game_started
+	if current_main_menu_instance.has_signal("start_game"): 
+		current_main_menu_instance.start_game.connect(_on_game_start)
 
 func _on_game_start(): # 开始游戏
-	if current_main_menu_instance:
-		current_main_menu_instance.queue_free()
-		current_main_menu_instance = null
-	load_level(first_level) # 加载第一关
+	current_main_menu_instance.queue_free()
+	current_main_menu_instance = null
+	
+	load_level(first_level) # 加载初始场景
+	load_hud() # 加载战斗ui
 
 func load_level(level_packed: PackedScene): # 加载关卡
 	_clear_world() # 清空旧关卡
-	if level_packed: # 实例化新关卡
-		var level_instance = level_packed.instantiate()
-		world_container.call_deferred("add_child", level_instance)
+	var level_instance = level_packed.instantiate() # 实例化新关卡
+	world_container.call_deferred("add_child", level_instance)
+
+func load_hud(): # 加载HUD
+	var ui_instance = hud_scene.instantiate()
+	hud.add_child(ui_instance)
 
 func _on_global_level_changed(next_scene_path: String, target_point_name: String): # 全局关卡切换
 	_clear_world() # 清理旧关卡
 	var new_level = load(next_scene_path) # 加载新关卡
-	if new_level:
-		var level_instance = new_level.instantiate()
-		world_container.add_child(level_instance)
+	var level_instance = new_level.instantiate()
+	world_container.add_child(level_instance)
 		
-		await get_tree().physics_frame # 等待物理帧，确保新地图的碰撞体和玩家都已初始化
+	await get_tree().physics_frame # 等待物理帧，确保新地图的碰撞体和玩家都已初始化
 		
-		var entry_point = level_instance.find_child(target_point_name) # 在新关卡寻找名字匹配的节点
-		if is_instance_valid(GlobalPlayerManager.player): # 安全检查，确保玩家实例存在，且没有被释放
-			GlobalPlayerManager.player.global_position = entry_point.global_position
+	var entry_point = level_instance.find_child(target_point_name) # 在新关卡寻找名字匹配的节点
+	if is_instance_valid(GlobalPlayerManager.player): # 安全检查，确保玩家实例存在，且没有被释放
+		GlobalPlayerManager.player.global_position = entry_point.global_position
 
 func _on_player_died(): # 玩家死亡处理
-	if game_over_menu_scene: # 实例化GameOver界面
-		current_game_over_menu = game_over_menu_scene.instantiate()
-		gui.add_child(current_game_over_menu)
-		if current_game_over_menu.has_signal("respawn_requested"): # 连接UI的重生信号
-			current_game_over_menu.respawn_requested.connect(_on_respawn)
+	hud.visible = false
+	current_game_over_menu = game_over_menu_scene.instantiate() # 实例化GameOver界面
+	gui.add_child(current_game_over_menu)
+	if current_game_over_menu.has_signal("respawn_requested"): # 连接UI的重生信号
+		current_game_over_menu.respawn_requested.connect(_on_respawn)
 
 func _on_respawn(): # 重生逻辑
 	current_game_over_menu.queue_free() # 清除Game Over界面
@@ -69,5 +78,8 @@ func _on_respawn(): # 重生逻辑
 		player.global_position = spawn_point.global_position # 搬运玩家
 		player.revive() # 唤醒玩家
 
-func _clear_world(): # 辅助函数：清空世界容器
+func _clear_world(): # 清空世界容器
 	for child in world_container.get_children(): child.queue_free()
+
+func _clear_hud(): # 清理hud
+	for child in hud.get_children(): child.queue_free()
